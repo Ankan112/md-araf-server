dddcd158a2f66770cefb3c747a6bcf87d130e563
Ankan112 <ankanhalder112@gmail.com>
2022-11-10 16:03:14 +0600

    mongodb problem solved

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
 const app = express();
 require('dotenv').config()
 
